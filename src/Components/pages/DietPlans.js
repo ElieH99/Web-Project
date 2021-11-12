50b3fcf320c663c384f7ff3fb2f09c27a4623063
michel.tadros@net.usj.edu.lb <michel.tadros@net.usj.edu.lb>
2021-11-12 18:24:05 +0200

    Small change

---
+++ src/Components/pages/DietPlans.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+
+
+function DietPlans() {
+  return (
+    <>
+      <h1>Choose an appropriate diet from the following</h1>
+    </>
+  );
+}
+
+export default DietPlans;

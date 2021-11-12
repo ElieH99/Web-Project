63272e05eeeea92f740687d3f967d4e77913d14a
michel.tadros@net.usj.edu.lb <michel.tadros@net.usj.edu.lb>
2021-11-12 20:59:39 +0200

    Added workout css still working

--- src/Components/pages/WorkoutPlans.js
+++ src/Components/pages/WorkoutPlans.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import '../../App.css';
+import './WorkoutPlans.css'
+
 
 
 function WorkoutPlans() {
   return (
     <>
+     <div className='workout-container'>
       <h1>What is your goal</h1>
+      <p>We recommend checking with a professional trainer before choosing a plan</p>
+      <div class="row">
+    <div class='column'>
+    <h2>Lose weight</h2>
+    <p>description of workout</p>
+    </div>
+    <div class="column">
+    <h2>Gain weight</h2>
+    <p>description of workout</p>
+    </div>
+    <div class="column">
+    <h2>Stay in shape</h2>
+    <p>description of workout</p>
+    </div>
+    <div class="column">
+    <h2>Build muscles</h2>
+    <p>description of workout</p>
+    </div>
+    <div class="column">
+    <h2>Toning</h2>
+    <p>description of workout</p>
+    </div>
+    </div>
+
+      </div>
     </>
   );
 }

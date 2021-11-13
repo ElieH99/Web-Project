8d338db818dc6569b10a6f9270e95159c2339791
michel.tadros@net.usj.edu.lb <michel.tadros@net.usj.edu.lb>
2021-11-13 18:29:05 +0200

    Finished plans page

--- src/Components/pages/DietPlans.js
+++ src/Components/pages/DietPlans.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import '../../App.css';
+import './DietPlans.css';
 
 
 function DietPlans() {
   return (
     <>
-      <h1>Choose an appropriate diet from the following</h1>
+    <div className='dietback'>
+       <div className='diet-title'>
+      <h1>A proper diet is as important as a proper workout!</h1>
+      <p>We recommend consulting a dieticien before choosing a diet</p>
+      </div>
+      <div class="drow">
+    <div class='dcolumn'>
+    <button>
+    <h2> Low calorie </h2>
+    <p>A low calorie diet gives a target intake of calories each day.Provides a steady way to achieve weight loss</p>
+    </button>
+    </div>
+
+    <div class="dcolumn">
+    <button>
+    <h2>Low-carbohydrate</h2>
+    <p>Low-carbohydrate diets involve restricting how many carbohydrates you eat. </p>
+    </button>
+    </div>
+    <div class="dcolumn">
+    <button className="buttonwork">
+    <h2>Low fat</h2>
+    <p>A low-fat diet is one that restricts fat. Intended to reduce the occurrence of conditions such as heart disease and obesity</p>
+    </button>
+    </div>
+    <div class="dcolumn">
+    <button>
+    <h2>Balanced </h2>
+    <p>A balanced diet consists of the proper quantities an proportions of foods to maintain health or growth </p>
+    </button>
+    </div>
+    <div class="dcolumn">
+    <button>
+    <h2>Weight Gain</h2>
+    <p>Increases your daily calorie intake to put on more weight by eating foods that are high in calories. </p>
+    </button>
+    </div>
+      </div>
+</div>
+
     </>
   );
 }
--- src/Components/pages/WorkoutPlans.js
+++ src/Components/pages/WorkoutPlans.js
@@ -7,33 +7,46 @@ import './WorkoutPlans.css'
 function WorkoutPlans() {
   return (
     <>
-     <div className='workout-container'>
-      <h1>What is your goal</h1>
+    <div className='workback'>
+       <div className='workout-title'>
+      <h1>Your journey starts here!</h1>
       <p>We recommend checking with a professional trainer before choosing a plan</p>
+      </div>
       <div class="row">
     <div class='column'>
-    <h2>Lose weight</h2>
-    <p>description of workout</p>
-    </div>
-    <div class="column">
-    <h2>Gain weight</h2>
-    <p>description of workout</p>
-    </div>
-    <div class="column">
-    <h2>Stay in shape</h2>
-    <p>description of workout</p>
-    </div>
-    <div class="column">
-    <h2>Build muscles</h2>
-    <p>description of workout</p>
-    </div>
-    <div class="column">
-    <h2>Toning</h2>
-    <p>description of workout</p>
-    </div>
+    <button>
+    <h2> Lose weight </h2>
+    <p>Losing weight is easier said than done, and there isn’t a magic pill to take off pounds. Instead, you have to burn more calories than you take in</p>
+    </button>
     </div>
 
+    <div class="column">
+    <button>
+    <h2>Gain weight</h2>
+    <p>Just as exercise can help people lose weight, it can also help others gain weight in a healthy way.</p>
+    </button>
+    </div>
+    <div class="column">
+    <button className="buttonwork">
+    <h2>Stay in shape</h2>
+    <p>Middle ground between losing and gaining weight. This workout is perfect for people happy with their body and want to  be more active.</p>
+    </button>
+    </div>
+    <div class="column">
+    <button>
+    <h2>Bodybuilding</h2>
+    <p>Bodybuilding  is the process of enhancing the body's muscle and symmetry and the promotion of overall health and fitness</p>
+    </button>
+    </div>
+    <div class="column">
+    <button>
+    <h2>Toning</h2>
+    <p>Toning exercises are physical exercises that are used with the aim of developing a physique with an emphasis on muscle shape not size</p>
+    </button>
+    </div>
       </div>
+</div>
+
     </>
   );
 }

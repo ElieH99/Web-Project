03aa22d63c8fa0bb49fc251f6fbf7249d15841a4
ElieH99 <elie.hanna5@net.usj.edu.lb>
2021-11-14 02:12:16 +0200

    Added links to trainers

--- src/Components/pages/Trainers.js
+++ src/Components/pages/Trainers.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../App.css';
 import "../pages/Trainers.css";
 import jeff from '../../images/jeff.jpg';
-import candice from '../../images/Candice.png';
+import michele from '../../images/Candice.png';
 import jen from '../../images/Jen.png';
 import simon from '../../images/simon.png';
 
@@ -35,21 +35,46 @@ function Trainers(){
 
                 </div>
                 <div class="team_card">
-                    <p>Candice</p>
-                    <img src={candice} alt="person" class="trainer_image" />
+                    <p>Michele</p>
+                    <div class="share">
+                    <a href="https://www.facebook.com/FitnessMichelle" target="blank" class="fab fa-facebook-f "/>
+                    <a href="https://twitter.com/Michelle_Lewin" target="blank" class="fab fa-twitter "/>
+                    <a href="https://www.instagram.com/michelle_lewin/" target="blank" class="fab fa-instagram "/>
+                    <a href="https://www.youtube.com/channel/UCXOF8RQ_v52K1uq6m_rMy1w" target="blank" class="fab fa-youtube"/> 
+                    </div>
+                    <img src={michele} alt="person" class="trainer_image"/>
                 </div>
+                
                 <div class="team_card">
                     <p>Jen</p>
+                    <div class="share">
+                    <a href="https://www.facebook.com/JenSelter" target="blank" class="fab fa-facebook-f "/>
+                    <a href="https://twitter.com/JenSelter" target="blank" class="fab fa-twitter "/>
+                    <a href="https://www.instagram.com/jenselter/"  target="blank" class="fab fa-instagram "/>
+                    <a href="https://www.youtube.com/channel/UCdfkczRQA4pRsGo30x4uIcA" target="blank" class="fab fa-youtube"/> 
+                    </div>
                     <img src={jen} alt="person" class="trainer_image" />
                 </div>
                 <div class="team_card">
                     <p>Jeff</p>
+                    <div class="share">
+                    <a href="https://www.facebook.com/athleanx" target="blank" class="fab fa-facebook-f "/>
+                    <a href=""  class="fab fa-twitter "/>
+                    <a href="https://www.instagram.com/athleanx" target="blank" class="fab fa-instagram "/>
+                    <a href="https://www.youtube.com/athleanx" target="blank" class="fab fa-youtube"/> 
+                    </div>
                     <img src={jeff} alt="person" class="trainer_image" />
                 </div>
                 
                 
                 <div class="team_card">
                     <p>Simon</p>
+                    <div class="share">
+                    <a href="#" class="fab fa-facebook-f "/>
+                    <a href="#" class="fab fa-twitter "/>
+                    <a href="#" class="fab fa-instagram "/>
+                    <a href="#" class="fab fa-youtube"/> 
+                    </div>
                     <img src={simon} alt="person" class="trainer_image" />
                 </div>
             </div>
